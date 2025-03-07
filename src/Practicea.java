76746a25e26e9f83e6b4fc60a927a7c19331c762
Shahrukh-codes <mahommadshahrukh7786@gmail.com>
2025-03-08 02:56:30 +0530

    Add string

--- src/Practicea.java
+++ src/Practicea.java
@@ -1,5 +1,11 @@
 public class Practicea {
     public static void main(String[] args) {
         System.out.println("I am Saim");
+        String s = "saim";
+
+        System.out.println( s.substring(0,1).toUpperCase()+s.substring(1,s.length()));
+
+
+
     }
 }
