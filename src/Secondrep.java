cdfb9f29aa3f3b795f7101d095b8623e5833d2a4
Shahrukh-codes <mahommadshahrukh7786@gmail.com>
2025-05-27 23:58:15 +0530

    add new file

---
+++ src/Secondrep.java
@@ -0,0 +1,7 @@
+public class Secondrep {
+    public static void main(String args[]){
+        String name = "Saim";
+        String name2 = "Hrida";
+        System.out.println(name +""+ name2);
+    }
+}
