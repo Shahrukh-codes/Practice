4bd579c131bdfd94a2b1ecd585a9eea7d4d26f59
Shahrukh-codes <mahommadshahrukh7786@gmail.com>
2025-06-27 23:51:19 +0530

    udate file

--- src/Abstr.java
+++ src/Abstr.java
@@ -1,5 +1,6 @@
 public class Abstr {
     public static void main(String args[]){
         System.out.println("Saim");
+        System.out.println("Hrida");
     }
 }
--- src/Secondrep.java
+++ src/Secondrep.java
@@ -2,7 +2,8 @@ public class Secondrep {
     public static void main(String args[]){
         String name = "Saim";
         String name2 = "Hrida";
-        System.out.println(name +""+ name2);
+        System.out.println();
+        System.out.print(name +""+ name2);
         System.out.println(name);
 
     }
